a8ebf650c9c18a06c8c83e214da17bde9e0fa7d3
Tyler Wellman <TangoKilo1985@gmail.com>
2022-07-11 08:30:17 -0400

    Working Gitz

---
+++ test.js
@@ -0,0 +1,33 @@
+//alert("Hello World!");
+
+//Variables
+
+var myVariableShaun;
+myVariableShaun = 10;
+
+var myVar2 = "hello";
+
+var myVar2 = "hello, again";
+
+var myVar2 = 5;
+
+varCustomerAge = 32;
+
+//Math Operators
+
+var myVar = 5;
+
+myVar + 10
+
+myVar = myVar + 10
+
+myVar - 3
+
+myVar +=
+myVar -=
+myVar /=
+myVar *=
+
+myVar += 1
+myVar ++
+++myVar
